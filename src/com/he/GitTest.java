3311128ffe0fa9ef12a158bd64a861da5cd29e24
beicheng222 <155640393+beicheng222@users.noreply.github.com>
2024-01-22 17:07:18 +0800

    Update GitTest.java

--- src/com/he/GitTest.java
+++ src/com/he/GitTest.java
@@ -24,5 +24,6 @@ public class GitTest {
         System.out.println("hot-fix test1");
         System.out.println("push test");
         System.out.println("push test1");
+        System.out.println("pull test")
     }
 }
