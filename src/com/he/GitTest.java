c05e15350a07ac1c8448223e9a5ae13692e1aeec
He <2556186159@qq.com>
2024-01-22 15:02:49 +0800

    first commit

---
+++ src/com/he/GitTest.java
@@ -0,0 +1,19 @@
+package com.he;
+
+import java.sql.SQLOutput;
+
+/**
+ * ClassName: GitTest
+ * Package: com.he
+ * Description:
+ *
+ * @Author 何新贵
+ * @Create 2024/1/22 14:59
+ * @Version1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.print("hexingui");
+    }
+}
